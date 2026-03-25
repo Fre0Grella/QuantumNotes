9251b0f15016920c215ca9e4c1df409fd2ce0d4c
Marco Galeri <marco.galeri@studio.unibo.it>
2026-03-25 17:55:03 +0100

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://fre0grella.github.io/QuantumNotes',
-  base: '/',
+  site: 'https://Fre0Grella.github.io',
+  base: '/QuantumNotes/',
   output: 'static',
 });
